c3579cc246119bd24b6c93d94ceaf5bb97cca8b2
Aaron Mahan <mahanability@gmail.com>
2020-08-09 08:36:56 -0700

    Removed some code

---
+++ script.py
@@ -0,0 +1,10 @@
+import requests
+
+
+r = requests.get("https://google.com")
+print(r.status_code)
+# print(greet('Hello!'))
+# print(greet('Aaron!'))
+
+# shift + option + F = refactor
+# python refactor: sort imports
